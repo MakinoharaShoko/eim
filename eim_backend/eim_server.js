2c351347ba9156df9359cea79f4c61da292c5feb
Kamome <makinoharashoko@outlook.com>
2021-11-18 14:50:07 +0800

    添加部分功能

--- eim_backend/eim_server.js
+++ eim_backend/eim_server.js
@@ -28,7 +28,7 @@ app.get('/getAllInfo/*', (req, res) => {
     eid = parseInt(eid);
     MongoClient.connect(MongoUrl,function (err,db) {
         if(err) throw err;
-        let dbo = db.db('userInfo');
+        let dbo = db.db('EIM');
         dbo.collection('users').find({eid:eid}).toArray(function (err,result) {
             if(err) throw err;
             res.send(result);
@@ -42,7 +42,7 @@ app.post(
         console.log(req.body);
         MongoClient.connect(MongoUrl, function(err, db) {
             if (err) throw err;
-            let dbo =db.db('userInfo');
+            let dbo =db.db('EIM');
             let obj = req.body;
             let UserCount;
             dbo.collection('users').find().count( function (err, count){
@@ -61,6 +61,7 @@ app.post(
     }
 )
 
+//处理用户发送信息的请求
 app.post(
     '/sendMes/*',(req,res) =>{
         console.log(req.body);
@@ -68,12 +69,87 @@ app.post(
             if (err) throw err;
             let dbo =db.db('EIM');
             let obj = req.body;
+            obj['sender'] = parseInt(obj['sender']);
+            obj['receiver'] = parseInt(obj['receiver']);
             dbo.collection("messages").insertOne(obj,function (err,result){
                 if (err) throw err;
                 db.close();
                 res.send('OK:\n'+obj.sender+': '+obj.message+'\n to '+obj.receiver);
             });
         });
+    }
+)
+
+// 处理用户请求添加好友的请求
+app.post(
+    '/addFriendReq/*',(req,res) =>{
+        console.log(req.body);
+        MongoClient.connect(MongoUrl, function(err, db) {
+            if (err) throw err;
+            let dbo =db.db('EIM');
+            let obj = req.body;
+            obj['sender'] = parseInt(obj['sender']);
+            obj['receiver'] = parseInt(obj['receiver']);
+            dbo.collection("addReq").insertOne(obj,function (err,result){
+                if (err) throw err;
+                db.close();
+                res.send('OK:\n'+obj.sender+'验证消息: '+obj.message+'\n to '+obj.receiver);
+            });
+        });
+    }
+)
+
+//更新好友关系的请求
+app.post(
+    '/updateFriendRel/*',(req,res) =>{
+        console.log(req.body);
+        MongoClient.connect(MongoUrl, function(err, db) {
+            if (err) throw err;
+            let dbo =db.db('EIM');
+            let obj = req.body;
+            obj['sender'] = parseInt(obj['sender']);
+            obj['receiver'] = parseInt(obj['receiver']);
+            dbo.collection("friendRel").insertOne(obj,function (err,result){
+                if (err) throw err;
+                db.close();
+                res.send('OK:\n'+obj.sender+'备注: '+obj.message+'\n to '+obj.receiver);
+            });
+        });
+    }
+)
+
+//主状态获取函数
+app.get(
+    '/getMainInfo/*',(req,res) =>{
+        let userEID = req.url.split('/')[2];
+        userEID = parseInt(userEID);
+        MongoClient.connect(MongoUrl,function (err,db){
+            if(err) throw err;
+            let returnMessage ={};
+            let dbo = db.db('EIM');
+            //获取个人信息
+            dbo.collection('users').find({eid:userEID}).toArray(function (err,result){
+                if(err)throw err;
+                returnMessage['userInfo'] = result;
+                //获取消息列表
+                dbo.collection('messages').find({$or:[{sender:userEID},{receiver:userEID}]}).toArray(function (err,result){
+                    if(err) throw err;
+                    returnMessage['messages'] = result;
+                    //获取好友申请列表
+                    dbo.collection('addReq').find({receiver:userEID}).toArray((error, result)=>{
+                        if(error) throw error;
+                        returnMessage['AddFriendReq']=result;
+                        //获取好友列表
+                        dbo.collection('friendRel').find({sender:userEID}).toArray((err,result)=>{
+                            if(err) throw err;
+                            returnMessage['friends'] = result;
+                            db.close();
+                            res.send(returnMessage);
+                        })
+                    })
+                })
+            })
+        })
     }
 )
 
