9ef5667d0bcc332daf22fe907cf118121e1fcffb
Kamome <makinoharashoko@outlook.com>
2021-12-10 13:49:05 +0800

    将启动服务器放到package

--- eim_backend/eim_server.js
+++ eim_backend/eim_server.js
@@ -39,182 +39,176 @@ app.get('/getAllInfo/*', (req, res) => {
     })
 });
 
-app.post(
-    '/register/*', (req, res) => {
-        console.log(req.body);
-        MongoClient.connect(MongoUrl, function (err, db) {
+app.post('/register/*', (req, res) => {
+    console.log(req.body);
+    MongoClient.connect(MongoUrl, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('EIM');
+        let obj = req.body;
+        let UserCount;
+        dbo.collection('users').find().count(function (err, count) {
             if (err) throw err;
-            let dbo = db.db('EIM');
-            let obj = req.body;
-            let UserCount;
-            dbo.collection('users').find().count(function (err, count) {
+            UserCount = count;
+            obj['eid'] = 10000 + count;
+            dbo.collection("users").insertOne(obj, function (err, result) {
                 if (err) throw err;
-                UserCount = count;
-                obj['eid'] = 10000 + count;
-                dbo.collection("users").insertOne(obj, function (err, result) {
-                    if (err) throw err;
-                    console.log("用户注册成功");
-                    db.close();
-                    res.send('注册成功，你的EID是 ' + obj['eid']);
-                });
+                console.log("用户注册成功");
+                db.close();
+                res.send('注册成功，你的EID是 ' + obj['eid']);
             });
         });
+    });
 
-    }
+}
 )
 
-app.post(
-    '/login/', (req, res) => {
-        let userEID = parseInt(req.body['userID']);
-        let pwd = req.body['pwd'];
-        MongoClient.connect(MongoUrl, (err, db) => {
-            if (err) throw err;
-            let dbo = db.db('EIM');
-            dbo.collection('users').find({ eid: userEID, pwd: pwd }).toArray((err, result) => {
-                if (result.length !== 0) {
-                    res.send('OK');
-                } else {
-                    res.send('error' + ' data is' + `id:${userEID},pwd:${pwd}`);
-                }
-                db.close();
-            })
+app.post('/login/', (req, res) => {
+    let userEID = parseInt(req.body['userID']);
+    let pwd = req.body['pwd'];
+    MongoClient.connect(MongoUrl, (err, db) => {
+        if (err) throw err;
+        let dbo = db.db('EIM');
+        dbo.collection('users').find({ eid: userEID, pwd: pwd }).toArray((err, result) => {
+            if (result.length !== 0) {
+                res.send('OK');
+            } else {
+                res.send('error' + ' data is' + `id:${userEID},pwd:${pwd}`);
+            }
+            db.close();
         })
-    }
+    })
+}
 )
 
 //处理用户发送信息的请求
-app.post(
-    '/sendMes/*', (req, res) => {
-        console.log(req.body);
-        MongoClient.connect(MongoUrl, function (err, db) {
+app.post('/sendMes/*', (req, res) => {
+    console.log(req.body);
+    MongoClient.connect(MongoUrl, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('EIM');
+        let obj = req.body;
+        obj['sender'] = parseInt(obj['sender']);
+        obj['receiver'] = parseInt(obj['receiver']);
+        dbo.collection("messages").insertOne(obj, function (err, result) {
             if (err) throw err;
-            let dbo = db.db('EIM');
-            let obj = req.body;
-            obj['sender'] = parseInt(obj['sender']);
-            obj['receiver'] = parseInt(obj['receiver']);
-            dbo.collection("messages").insertOne(obj, function (err, result) {
-                if (err) throw err;
-                db.close();
-                res.send('OK:\n' + obj.sender + ': ' + obj.message + '\n to ' + obj.receiver);
-            });
+            db.close();
+            res.send('OK:\n' + obj.sender + ': ' + obj.message + '\n to ' + obj.receiver);
         });
-    }
+    });
+}
 )
 
 // 处理用户请求添加好友的请求
-app.post(
-    '/addFriendReq/*', (req, res) => {
-        console.log(req.body);
-        MongoClient.connect(MongoUrl, function (err, db) {
+app.post('/addFriendReq/*', (req, res) => {
+    console.log(req.body);
+    MongoClient.connect(MongoUrl, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('EIM');
+        let obj = req.body;
+        obj['sender'] = parseInt(obj['sender']);
+        obj['receiver'] = parseInt(obj['receiver']);
+        dbo.collection("addReq").insertOne(obj, function (err, result) {
             if (err) throw err;
-            let dbo = db.db('EIM');
-            let obj = req.body;
-            obj['sender'] = parseInt(obj['sender']);
-            obj['receiver'] = parseInt(obj['receiver']);
-            dbo.collection("addReq").insertOne(obj, function (err, result) {
-                if (err) throw err;
-                db.close();
-                res.send('OK:\n' + obj.sender + '验证消息: ' + obj.message + '\n to ' + obj.receiver);
-            });
+            db.close();
+            res.send('OK:\n' + obj.sender + '验证消息: ' + obj.message + '\n to ' + obj.receiver);
         });
-    }
+    });
+}
 )
 
 //更新好友关系的请求
-app.post(
-    '/updateFriendRel/*', (req, res) => {
-        console.log(req.body);
-        MongoClient.connect(MongoUrl, function (err, db) {
-            if (err) throw err;
-            let dbo = db.db('EIM');
-            let obj = req.body;
-            obj['sender'] = parseInt(obj['sender']);
-            obj['receiver'] = parseInt(obj['receiver']);
-            dbo.collection("friendRel").updateOne({ sender: obj.sender, receiver: obj.receiver },
-                { $set: obj },
-                { upsert: true },
-                function (err, result) {
-                    if (err) throw err;
-                    res.send('OK:\n' + obj.sender + '备注: ' + obj.message + '\n to ' + obj.receiver);
-                    dbo.collection('addReq').deleteMany(
-                        { sender: obj.receiver, receiver: obj.sender }, (err, result) => {
-                            if (err) throw err;
-                            db.close();
-                        });
-                });
-        });
-    }
+app.post('/updateFriendRel/*', (req, res) => {
+    console.log(req.body);
+    MongoClient.connect(MongoUrl, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('EIM');
+        let obj = req.body;
+        obj['sender'] = parseInt(obj['sender']);
+        obj['receiver'] = parseInt(obj['receiver']);
+        dbo.collection("friendRel").updateOne({ sender: obj.sender, receiver: obj.receiver },
+            { $set: obj },
+            { upsert: true },
+            function (err, result) {
+                if (err) throw err;
+                res.send('OK:\n' + obj.sender + '备注: ' + obj.message + '\n to ' + obj.receiver);
+                dbo.collection('addReq').deleteMany(
+                    { sender: obj.receiver, receiver: obj.sender }, (err, result) => {
+                        if (err) throw err;
+                        db.close();
+                    });
+            });
+    });
+}
 )
 
 //主状态获取函数
-app.get(
-    '/getMainInfo/*', (req, res) => {
-        let userEID = req.url.split('/')[2];
-        userEID = parseInt(userEID);
-        MongoClient.connect(MongoUrl, function (err, db) {
-            if (err) throw err;
-            let returnMessage = {};
-            let dbo = db.db('EIM');
-            //执行查询
-            Promise.all([getUserInfo(), getMessage(), getFriendReq(), getFriendList()]).then(() => { closeAndSend() })
+app.get('/getMainInfo/*', (req, res) => {
+    let userEID = req.url.split('/')[2];
+    userEID = parseInt(userEID);
+    MongoClient.connect(MongoUrl, function (err, db) {
+        if (err) throw err;
+        let returnMessage = {};
+        let dbo = db.db('EIM');
+        //执行查询
+        Promise.all([getUserInfo(), getMessage(), getFriendReq(), getFriendList()]).then(() => { closeAndSend() })
 
-            //以下是相关函数定义
-            function getUserInfo() {
-                //获取个人信息
-                return new Promise((resolve, reject) => {
-                    dbo.collection('users').find({ eid: userEID }).toArray((err, result) => {
-                        if (err) throw err;
-                        returnMessage['userInfo'] = result;
-                        resolve();
-                    })
-                })
-
-            }
-            function getMessage() {
-                //获取消息列表
-                return new Promise((resolve, reject) => {
-                    dbo.collection('messages').find({ $or: [{ sender: userEID }, { receiver: userEID }] }).toArray((err, result) => {
-                        if (err) throw err;
-                        returnMessage['messages'] = result;
-                        resolve();
-                    })
-                })
-
-            }
-
-            function getFriendReq() {
-                //获取好友申请列表
-                return new Promise((resolve, reject) => {
-                    dbo.collection('addReq').find({ receiver: userEID }).toArray((error, result) => {
-                        if (error) throw error;
-                        returnMessage['AddFriendReq'] = result;
-                        resolve();
-                    })
-                })
-
-            }
-            function getFriendList() {
-                //获取好友列表
-                return new Promise((resolve, reject) => {
-                    dbo.collection('friendRel').find({ sender: userEID }).toArray((err, result) => {
-                        if (err) throw err;
-                        returnMessage['friends'] = result;
-                        resolve();
-                    })
-                })
-
-            }
-
-            function closeAndSend() {
-                return new Promise((resolve, reject) => {
-                    db.close();
-                    res.send(returnMessage);
+        //以下是相关函数定义
+        function getUserInfo() {
+            //获取个人信息
+            return new Promise((resolve, reject) => {
+                dbo.collection('users').find({ eid: userEID }).toArray((err, result) => {
+                    if (err) throw err;
+                    returnMessage['userInfo'] = result;
                     resolve();
                 })
-            }
+            })
 
-        })
-    }
+        }
+        function getMessage() {
+            //获取消息列表
+            return new Promise((resolve, reject) => {
+                dbo.collection('messages').find({ $or: [{ sender: userEID }, { receiver: userEID }] }).toArray((err, result) => {
+                    if (err) throw err;
+                    returnMessage['messages'] = result;
+                    resolve();
+                })
+            })
+
+        }
+
+        function getFriendReq() {
+            //获取好友申请列表
+            return new Promise((resolve, reject) => {
+                dbo.collection('addReq').find({ receiver: userEID }).toArray((error, result) => {
+                    if (error) throw error;
+                    returnMessage['AddFriendReq'] = result;
+                    resolve();
+                })
+            })
+
+        }
+        function getFriendList() {
+            //获取好友列表
+            return new Promise((resolve, reject) => {
+                dbo.collection('friendRel').find({ sender: userEID }).toArray((err, result) => {
+                    if (err) throw err;
+                    returnMessage['friends'] = result;
+                    resolve();
+                })
+            })
+
+        }
+
+        function closeAndSend() {
+            return new Promise((resolve, reject) => {
+                db.close();
+                res.send(returnMessage);
+                resolve();
+            })
+        }
+
+    })
+}
 )
 
 app.post('/delFriendRel', (req, res) => {
